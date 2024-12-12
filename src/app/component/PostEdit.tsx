847930b075e2932db90f4517c25044546bf27223
Kshitij852 <kshitij.singh@atomostech.com>
2024-12-12 12:39:17 +0530

    added changes

---
+++ src/app/component/PostEdit.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { Component, createRef } from "react";
+
+import Editor from "./Editor";
+
+interface Props {}
+
+interface State {
+  content: string;
+}
+
+class PostEdit extends Component<Props, State> {
+  editorRef: any = createRef();
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      content: "Test",
+    };
+  }
+
+  save() {
+    const contents = this.editorRef.current.editor.getContents();
+    console.log("save", contents);
+  }
+
+  render() {
+    return (
+      <div>
+        <Editor
+          ref={this.editorRef}
+          contents={this.state.content}
+          onSave={this.save.bind(this)}
+        ></Editor>
+
+        <button onClick={() => this.save()}>
+          <span>Save</span>
+        </button>
+      </div>
+    );
+  }
+}
+
+export default PostEdit;
